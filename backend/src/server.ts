96828cde56e2308b7dfefbb565a92e3d12ea317e
Jaska <pusanteemu@gmail.com>
2023-07-11 12:01:55 +0300

    Add bcrypt package and implement login
    also reformat some of messy code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,19 @@ import { PrismaClient } from '@prisma/client';
 import { error } from 'console'
 import { createSchema, createYoga } from 'graphql-yoga'
 import { createServer } from 'http'
+import { hash, compare } from "bcrypt"
+
 const jwt = require('jsonwebtoken');
 
 const secretKey = 'yourSecretKey';
 
-const database: any ={
-  users:[{
-    
-  }
-   
+const database: any = {
+	users: [{
 
-]
+	}
+
+
+	]
 
 }
 
@@ -26,22 +28,20 @@ type Context = {
 
 const yoga = createYoga({
 	context: (req): Context => {
-	 	const token = req.request.headers.get("authorization")
+		const token = req.request.headers.get("authorization")
 		const tokenArray = token.split(" ");
-    const actualToken = tokenArray[1];
+		const actualToken = tokenArray[1];
 		let user
-    console.log(actualToken)
-    console.log("apina")
-
-
+		console.log(actualToken)
+		console.log("apina")
 
 		if (actualToken) {
 			try {
 				const decoded = jwt.verify(actualToken, secretKey)
 				console.log("decoded token", decoded)
 				user = decoded
-			} catch(err) {
-				console.log("eeeeeeee",err)
+			} catch (err) {
+				console.log("eeeeeeee", err)
 			}
 		}
 
@@ -50,106 +50,118 @@ const yoga = createYoga({
 			prisma: prisma
 		}
 	},
-
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
+	schema: createSchema({
+		typeDefs: /* GraphQL */ `
       
-      type User {
-        id: String!
-        email: String!
-        username: String!
-      }
-
-	    type Viewer {
-		  user: User
-	    }
-
-      type LoginResponse {
-        token: String!
-        viewer: Viewer!
-      }
-      
-      type Query {
-        email: String!
-        username: String!
-		    viewer: Viewer! 
-      }
-      
-      type Mutation {
-        register(email: String!, username: String!, password: String!): User
-        login(username: String!, password: String!): LoginResponse!
-      }
-     
-    `,
-   resolvers: {
-	  Query: {
-		viewer: (root, args, ctx: any) => {
-			return {
-				user: ctx.user
-			}
-		},
-		user: (root, args, ctx: Context) => {
-			return ctx.prisma.user.findFirst({
-				where: {
-					id: args.userId
-				}
-			})
+		type User {
+			id: String!
+			email: String!
+			username: String!
 		}
-	  },
-      Mutation: {
-        login: (root, args) =>{
-          
-         console.log(args)
-         console.log(database)
 
-         const user = database.users.find(user => user.username === args.username && user.password === args.password )
-        console.log(user)
+		type Viewer {
+			user: User
+		}
 
-        const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
+		type LoginResponse {
+			token: String!
+			viewer: Viewer!
+		}
+		
+		type Query {
+			email: String!
+			username: String!
+				viewer: Viewer! 
+		}
+		
+		type Mutation {
+			register(email: String!, username: String!, password: String!): User
+			login(username: String!, password: String!): LoginResponse!
+		}
+     	`,
+		resolvers: {
+			Query: {
+				viewer: (root, args, ctx: any) => {
+					return {
+						user: ctx.user
+					}
+				},
+				user: (root, args, ctx: Context) => {
+					return ctx.prisma.user.findFirst({
+						where: {
+							id: args.userId
+						}
+					})
+				}
+			},
+			Mutation: {
+				login: async (root, args, ctx: Context) => {
 
-        console.log('Generated JWT token:', token);
+					console.log(args)
+					console.log(database)
 
-         return {
-			  token: token,
-			  viewer: {
-				user: user
+					const user = await ctx.prisma.user.findFirst({
+						where: {
+							username: args.username,
+							passwordHash: args.password
+						}
+					})
+
+					if (!user) {
+						throw new Error('No such user found')
+					}
+
+					if (await compare(args.password, user.passwordHash) === false) {
+						throw new Error('Invalid password')
+					}
+
+					console.log(user)
+
+					const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
+
+					console.log('Generated JWT token:', token);
+
+					return {
+						token: token,
+						viewer: {
+							user: user
+						}
+					}
+
+				},
+				register: (root, args) => {
+					const newuser = {
+						id: database.users.length + 1,
+						email: args.email,
+						username: args.username,
+						password: args.password
+
+					};
+					const user = database.users.find(user => user.username === args.username || user.email === args.email)
+
+
+					if (user == null) {
+
+						database.users.push(newuser)
+
+
+					} else {
+						throw new Error("username or email is taken !")
+					}
+
+
+
+				}
+
 			}
-		 }
-         
-        },
-        register: (root, args) => {
-          const newuser ={
-            id: database.users.length + 1,
-            email:args.email,
-            username:args.username,
-            password:args.password
-            
-          };
-          const user = database.users.find(user => user.username === args.username || user.email === args.email)
-         
-
-          if(user == null){
-
-            database.users.push(newuser)
-
-
-          }else{
-            throw new Error("username or email is taken !")
-          }
-          
-         
-          
-        } 
-        
-      }
-    }
-  })
+		}
+	})
 })
 
 const server = createServer(yoga)
 
 server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+	console.info('Server is running on http://localhost:4000/graphql')
 })
 
 
