e8dd8b806e19348c6d963dcef8740bfe8cb49d34
jaska <pusanteemu@gmail.com>
2024-01-09 10:51:06 +0200

    add resource example

--- web/src/resources.tsx
+++ web/src/resources.tsx
@@ -1,8 +1,39 @@
 
 export const ResourcesPage = () => {
 	return (
-		<div>
-			Resources page
+		<div style={{ backgroundColor: "white", display: "inline-block" }}>
+			<table>
+				<thead>
+					<th>Resource</th>
+					<th>Amount</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							Total Population
+						</td>
+						<td>
+							100
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Military	
+						</td>
+						<td>
+							20	
+						</td>	
+					</tr>
+					<tr>
+						<td>
+							Wood
+						</td>
+						<td>
+							42
+						</td>
+					</tr>	
+				</tbody>
+			</table>
 		</div>
 	)
 }
