052dfda376a5903f3046d51f428cd888ccdaa38b
jaska <pusanteemu@gmail.com>
2024-02-12 09:54:48 +0200

    Implement SlideNavigation component

--- ios/Darkforest/ContentView.swift
+++ ios/Darkforest/ContentView.swift
@@ -46,33 +46,145 @@ struct ActivityItemView: View {
     }
 }
 
-struct ContentView: View {
+struct TechTree: View {
+	var body: some View {
+		Text("Tech Tree")
+	}
+}
+
+struct ActivityView: View {
     var items = [
         ActivityItem(name: "Library", completedAt: Calendar.current.date(byAdding: .minute, value: 5, to: Date())!),
         ActivityItem(name: "Barrack", completedAt: Calendar.current.date(byAdding: .minute, value: 5, to: Date())!)
     ]
-    
-    var body: some View {
-        ZStack {
-            Image("background")
-                .resizable()
-                .clipped()
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!!!!!")
-                    .foregroundColor(.white)
+
+	var body: some View {
+		VStack {
                 List(items) {item in
                     ActivityItemView(item: item)
                 }
             }
             .frame(maxWidth: .infinity) // This line ensures the VStack takes the full width
             .padding()
+	}
+}
+
+struct SlideNavigation: View {
+    var pages: [AnyView]
+    @State private var offset = 0.0
+    @State private var lastOffset = 0.0
+    @State private var pageIndex = 0
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .clipped()
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                HStack {
+                    ForEach(0..<pages.count, id: \.self) { index in
+                        VStack {
+                            pages[index]
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                        }
+                        //.background()
+                    }
+                }
+            }
+            .offset(x: lastOffset + offset - (UIScreen.main.bounds.width * Double(pageIndex)))
+            .gesture(
+                DragGesture()
+                    .onChanged({ v in
+                        offset = v.translation.width
+                    })
+                    .onEnded {gesture in
+                        if pageIndex < pages.count - 1 && offset < -UIScreen.main.bounds.width / 2 {
+                            withAnimation {
+                                pageIndex += 1
+                            }
+                        }
+                        
+                        if pageIndex > 0 && offset > UIScreen.main.bounds.width / 2 {
+                            withAnimation {
+                                pageIndex -= 1
+                            }
+                        }
+                        
+                        lastOffset = offset + lastOffset
+                        offset = 0.0
+                        
+                        withAnimation {
+                            lastOffset = 0
+                        }
+                    }
+            )
+            //.frame(width: geometry.size.width, height: geometry.size.height)
         }
     }
 }
+
+struct ContentView: View {
+    @State private var offset = 0.0
+	@State private var selection: String = "Activity"
+    @GestureState private var dragOffset = CGSize.zero
+    var body: some View {
+        SlideNavigation(pages: [
+            AnyView(ActivityView()),
+            AnyView(TechTree())
+        ])
+        
+    }
+}
+
+/*ZStack {
+    Image("background")
+        .resizable()
+        .clipped()
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    ActivityView()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.red) // Background for visibility
+        //.offset(x: selection == "Activity" ? 0 : -UIScreen.main.bounds.width)
+        .offset(x: dragOffset.width)
+
+    TechTree()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.blue) // Background for visibility
+        //.offset(x: selection == "TechTree" ? UIScreen.main.bounds.width : 0)
+        .offset(x: dragOffset.width - (selection == "TechTree" ? UIScreen.main.bounds.width : 0))
+}
+.gesture(
+    DragGesture()
+        .updating($dragOffset, body: { (value, state, _) in
+            state = value.translation
+            print("Drag offset: \(value.translation)")
+        })
+        .onEnded {gesture in
+            /*if gesture.translation.width < -100 {
+                selection = "Activity"
+            } else if gesture.translation.width > 100 {
+                selection = "TechTree"
+            }*/
+            
+        }
+)*/
+/*.overlay(
+    // Toggle button to switch views
+    VStack {
+        Spacer()
+        Button(action: {
+            // Toggle the state to switch views
+            selection = selection == "Activity" ? "TechTree" : "Activity"
+        }) {
+            Text(selection == "Activity" ? "Show Activity View" : "Show Tech Tree")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+        }
+        .padding()
+}, alignment: .bottom)*/
 
 #Preview {
     ContentView()
