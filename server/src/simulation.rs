f290c47567f74c45c44c12df2f7fcef8a88ef728
jaska <pusanteemu@gmail.com>
2024-02-23 10:43:51 +0200

    Add some structures

--- server/src/simulation.rs
+++ server/src/simulation.rs
@@ -1,6 +1,21 @@
+use crate::types::Building;
 use crate::types::GameWorld;
+use crate::types::Planet;
 
+fn simulate_building(building: &mut Building, delta_seconds: u32) {
+
+}
+
+fn simulate_planet(planet: &mut Planet, delta_seconds: u32) {
+	for unit in &planet.units {
+
+	}
+}
 
 pub fn simulate(world: &mut GameWorld, delta_seconds: u32) {
+	for planet in &world.planets {
+		for building in &planet.buildings {
 
+		}
+	}
 }
--- server/src/types.rs
+++ server/src/types.rs
@@ -1,16 +1,26 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub struct Coordinate {
+	pub planet: u32,
 	pub x: u32,
 	pub y: u32,
 }
 
+pub enum Location {
+	Building,
+	Coordinate(Coordinate)
+}
+
 pub struct Unit {
-	pub coordinate: Coordinate,
+	pub utype: u32,
+	pub location: Location,
+	pub moving_to: Option<Coordinate>,
 	pub attack_damage: u32,
 	pub health: u32,
 	pub accuracy: u32,
 	pub attack_range: u32,
+	pub move_speed: u32,
 }
 
 pub struct GameDate {
@@ -36,24 +46,34 @@ pub struct BuildingShape {
 	pub points: Vec<Coordinate>
 }
 
+pub struct ResourceProd {
+	pub resource: String,
+	pub rate: u32
+}
+
 pub struct Building {
+	pub owner: u32,
 	pub construction_done_at: Option<GameDate>,
 	pub health: u32,
-	pub shape: BuildingShape
+	pub shape: BuildingShape,
+	pub resource_prods: HashMap<String, u32>,
+	pub resource_stored: HashMap<String, u32>,
+	pub max_resource_storage: HashMap<String, u32>,
 }
 
 pub struct Player {
+	pub id: u32,
 	// What things player has found in the tech tree
 	pub tech_tree: HashSet<String>
 }
 
-pub struct Map {
+pub struct Planet {
 	pub buildings: Vec<Building>,
 	pub units: Vec<Unit>,
 }
 
 pub struct GameWorld {
-	pub map: Map,
+	pub planets: Vec<Planet>,
 	pub players: Vec<Player>,
 	pub date: GameDate
 }
